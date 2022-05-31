8859071514e872832e5b05bf5336c3961e887003
Taka005 <01taka005@gmail.com>
2022-05-31 22:18:12 +0900

    色々追加

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const config = require("./config.json");
 let temp = ["35.7", "35.8", "35.9","36.0", "36.1","36.2","36.3","36.4", "36.5", "36.6", "36.7", "36.8", "36.9", "37.0"]; 
 for(i=0;i<config.temp;i++){
-let random = Math.floor(Math.random() * temp.length);
-console.log(temp[random]);
+  let random = Math.floor(Math.random() * temp.length);
+  console.log(temp[random]);
+  try{
+    fs.appendFileSync(`./temp.txt`, `${temp[random]}\n`, 'utf8');
+  }catch(error){
+    console.log(error)
+  }
 }
