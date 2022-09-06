3f5e1944205c74405cf5435b4d379af358d2095a
Taka005 <01taka005@gmail.com>
2022-09-06 20:48:58 +0900

    追加

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,23 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
+let temp_data = ["35.7", "35.8", "35.9","36.0", "36.1","36.2","36.3","36.4", "36.5", "36.6", "36.7", "36.8", "36.9", "37.0"]; 
 
 form.addEventListener("submit", function (event){
     event.preventDefault();
-    let temp = input.value;
-    
-    if(!temp) return;
-    const li = document.createElement("li");
-    
-    li.innerText = temp;
-    li.classList.add("list-group-item")
-    
-    li.addEventListener("click", function (event) {
+    const temp = input.value;
+  
+    for(i=0;i<temp;i++){
+        const random = Math.floor(Math.random() * temp_data.length);
+        const li = document.createElement("li");
+        li.innerText = temp_data[random];
+        li.classList.add("list-group-item")  
+    }
+
+    li.addEventListener("click", function (event){
         event.preventDefault();
         li.remove();
     });
     
     ul.appendChild(li);
-    input.value = "";
 });
